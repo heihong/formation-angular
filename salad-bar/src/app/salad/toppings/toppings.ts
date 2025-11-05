306380f077eb7bdae1a2bb838904457a020f2cea
unknown <traingheihong@gmail.com>
2025-11-05 09:46:47 +0100

    feat: input classic child

--- salad-bar/src/app/salad/toppings/toppings.ts
+++ salad-bar/src/app/salad/toppings/toppings.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Topping } from '../../models/topping';
 
 @Component({
   selector: 'app-toppings',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './toppings.scss',
 })
 export class Toppings {
-
+  @Input() public toppings: Topping[] = []
 }
